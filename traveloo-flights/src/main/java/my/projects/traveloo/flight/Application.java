c07317241b5512c5fdb98fee6a7a806df5c1eff6
Jacopo Franzoi <jacopo.franzoi@gmail.com>
2020-09-17 23:01:22 +0200

    flights: added swagger

--- traveloo-flights/src/main/java/my/projects/traveloo/flight/Application.java
+++ traveloo-flights/src/main/java/my/projects/traveloo/flight/Application.java
@@ -7,6 +7,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 @EnableScheduling
@@ -24,5 +28,13 @@ public class Application {
     @Bean
     public Inventory inventory(Database database) {
         return new Inventory(database);
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30).select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 }
